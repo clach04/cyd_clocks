326da5ca6317aafe3115f6efb5dd671bf59c71a6
clach04 <clach04@gmail.com>
2024-12-21 12:50:44 -0800

    rainbow test added and gamma note

---
+++ 01-rainbow-test.py
@@ -0,0 +1,52 @@
+# 01-rainbow-test.py
+"""From https://github.com/dmccreary/micropython-clocks-and-watches/blob/main/src/kits/ili9341/01-rainbow-test.py
+Modified as did not work out of box for me
+"""
+# print out "Hello World!" using the rotation=3 using 32-bit high font
+# the default is white text on a black background
+from ili9341 import Display, color565
+import machine
+from machine import Pin, SPI
+
+WIDTH = 320
+HEIGHT = 240
+
+# Baud rate of 40000000 seems about the max
+spi = machine.SPI(1, baudrate=40000000, sck=machine.Pin(14), mosi=machine.Pin(13))
+# display = Display(spi, dc=Pin(4), cs=Pin(16), rst=Pin(17))
+# TODO see if can use lvgl API, which has different parameters
+# bgr-mode disabled for CYD2
+display = Display(
+            spi, dc=machine.Pin(2), cs=machine.Pin(15),
+            rst=machine.Pin(15),
+            width=320, height=240,  # ommit this and endup with square, with static on right hand side
+            bgr=False,
+            #gamma=True
+            gamma=False
+)
+
+backlight = machine.Pin(21, machine.Pin.OUT)
+backlight.on()  # TODO review PWM instead of on/off
+
+
+RED = color565(255,0,0)
+ORANGE = color565(255,128,0)
+YELLOW = color565(255,255,0)
+GREEN = color565(0,255,0)
+BLUE = color565(0,0,255)
+PURPLE = color565(255,0,255)
+WHITE = color565(255,255,255)
+BLACK = color565(0,0,0)
+
+display.clear()
+
+display.fill_rectangle(0,0, 50,HEIGHT, RED)
+display.fill_rectangle(50,0, 50,HEIGHT, ORANGE)
+display.fill_rectangle(100,0, 50,HEIGHT, YELLOW)
+display.fill_rectangle(150,0, 50,HEIGHT, GREEN)
+display.fill_rectangle(200,0, 50,HEIGHT, BLUE)
+display.fill_rectangle(250,0, 50,HEIGHT, PURPLE)
+display.fill_rectangle(300,0, 20,HEIGHT, WHITE)
+
+
+print('Done')
--- README.md
+++ README.md
@@ -9,6 +9,15 @@ screen. Relies on Micropython with library/libraries:
       * Original https://github.com/rdagger/micropython-ili9341
   * https://github.com/witnessmenow/ESP32-Cheap-Yellow-Display/
 
+NOTE on my device color reproduction is improved with changes in ILI9341 init,
+either pass in `gamma=False` or modify driver with changes from https://github.com/de-dh/ESP32-Cheap-Yellow-Display-Micropython-LVGL/tree/main/demo_no_lvgl:
+
+    self.write_cmd(self.GAMMASET, 0x02)
+    sleep(.1)
+    self.write_cmd(self.GAMMASET, 0x01)
+    # TODO another; sleep(.1)
+
+
 Demos:
 
   * tiny_8x8_clock_always_paint.py - 8x8 font, not really readable - basic test including ISO date display
