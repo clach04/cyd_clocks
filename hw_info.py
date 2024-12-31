39f31b3a0716f660170828dd4515f2f7429e593f
clach04 <clach04@gmail.com>
2024-12-30 18:49:18 -0800

    hw_info MAC display tweaks

--- hw_info.py
+++ hw_info.py
@@ -2,6 +2,11 @@
 """determine MicroPython (ESP32) hardware details
 """
 
+import os
+import platform
+import sys
+
+# MicroPython specific
 import bluetooth
 try:
     import esp
@@ -9,10 +14,13 @@ except ImportError:
     esp = None
 import machine
 import network
-import os
-import platform
-import sys
 
+
+def printable_mac(in_bytes, seperator=':'):
+    if seperator:
+        return seperator.join(['%02x' % x for x in in_bytes])
+    else:
+        return in_bytes.hex()
 
 print('os.uname %r' % (os.uname(),))
 print('platform.platform %r' % (platform.platform(),))
@@ -21,7 +29,10 @@ print('sys.platform %r' % (sys.platform,))
 print('sys.version %r' % (sys.version,))
 print('sys.version_info %r' % (sys.version_info,))
 print('machine.unique_id %r' % (machine.unique_id(),))
-print('machine.unique_id %r' % (machine.unique_id().hex(),))
+micropython_version = list(map(int, os.uname().release.split('.')))  # very complicated version of sys.implementation.version (which returns a tuple
+print('micropython_version %r' % (micropython_version,))
+if micropython_version > [1, 19, 1]:
+    print('machine.unique_id %r' % (machine.unique_id().hex(),))
 if esp:
     print('esp.flash_size() %r' % (esp.flash_size(),))
 # TODO determine why esp.flash_id() is  ESP8266 only
@@ -31,11 +42,13 @@ if esp:
 wlan_ap = network.WLAN(network.AP_IF)
 wlan_sta = network.WLAN(network.STA_IF)
 
-ap_addr = wlan_ap.config('mac').hex()  # MAC in bytes
-cl_addr = wlan_sta.config('mac').hex()  # MAC in bytes
+# MAC is in bytes
+cl_addr = printable_mac(wlan_sta.config('mac'))
+ap_addr = printable_mac(wlan_ap.config('mac'))
 
-print('AP MAC           %r' % (ap_addr,))
+print('Regular MAC      %r' % (wlan_sta.config('mac'),))
 print('Regular MAC      %r' % (cl_addr,))
+print('AP MAC           %r' % (ap_addr,))
 
 ble = bluetooth.BLE()  # defaults to addr_mode == PUBLIC 
 ble.active(True)
@@ -43,5 +56,23 @@ ble_pub_addr = ble.config('mac')  # MAC tuple, with MAC in bytes
 ble.active(False)
 
 print('BT-LE Public MAC %r' % (ble_pub_addr,))
-print('BT-LE Public MAC %r' % (ble_pub_addr[1].hex(),))
+if micropython_version > [1, 19, 1]:
+    print('BT-LE Public MAC %r' % (printable_mac(ble_pub_addr[1], seperator=''),))
+print('BT-LE Public MAC %r' % (printable_mac(ble_pub_addr[1]),))
 
+check_i2c_bus = False
+if check_i2c_bus:
+    # i2c
+    # SoftI2C() returns nothing on CYD1
+    # Not been able to init Hardware I2C() :-(
+    SCL_pin_number = 22
+    SDA_pin_number = 27
+
+    #i2c = machine.I2C(1, machine.Pin(SCL_pin_number), machine.Pin(SDA_pin_number), freq=400000)  # create I2C peripheral at frequency of 400kHz
+    #i2c = machine.SoftI2C(machine.Pin(SCL_pin_number), machine.Pin(SDA_pin_number), freq=400000)  # create I2C peripheral at frequency of 400kHz
+    #i2c = machine.SoftI2C(machine.Pin(SDA_pin_number), machine.Pin(SCL_pin_number), freq=400000)  # create I2C peripheral at frequency of 400kHz
+    #
+    #i2c = machine.SoftI2C(SDA_pin_number, SCL_pin_number)  # NOTE I think this is reversed.... CYD1 and original CYD2 Returns [8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119]
+    i2c = machine.SoftI2C(machine.Pin(SDA_pin_number), machine.Pin(SCL_pin_number), freq=400000)  # NOTE I think this is reversed... NOTE micropython_version v1.19.1 needs Pins (not numbers), v1.24.1 does NOT
+    for device_id in i2c.scan():
+        print('found %02x (%d)' % (device_id, device_id))
