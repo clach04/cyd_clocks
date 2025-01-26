541338f2ad9eb237373b51a927063b9171af77e7
clach04 <clach04@gmail.com>
2025-01-26 09:22:14 -0800

    Initial not complete nano rainbow demo #36

---
+++ nano_rainbow_test.py
@@ -0,0 +1,37 @@
+# rainbow-test.py
+# Sanity check rotation and colors
+
+from color_setup import ssd  # Create a display instance
+from color_setup import width as WIDTH, height as HEIGHT
+from gui.core.colors import RED, LIGHTGREEN, YELLOW, GREEN, BLUE, MAGENTA as PURPLE, WHITE, BLACK
+from gui.core.nanogui import refresh
+
+
+ORANGE = LIGHTGREEN  # FIXME
+
+
+def display_function():
+    refresh(ssd, True)  # Initialise and clear display.
+
+    #color_list = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE, BLACK]
+    color_list = [RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, WHITE]
+
+    # generate vertical bars
+    bar_width = int(WIDTH / len(color_list))
+    bar_height = HEIGHT
+
+    offset = 0
+    for color in color_list:
+        #FrameBuffer.rect(x, y, w, h, c[, f])¶
+        ssd.rect(offset, 0, bar_width, bar_height, color, True)  # Red square at top left
+        offset += bar_width
+    refresh(ssd)
+
+    print('Done')  # to serial port
+
+
+try:
+    display_function()
+finally:
+    # Leave screen alone/on for visual inspection - uncomment below to change that
+    pass
