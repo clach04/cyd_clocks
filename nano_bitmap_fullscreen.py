b09cc1f9fd09b20d7e76998e7cb44aa32b8a4ff8
clach04 <clach04@gmail.com>
2025-01-27 14:39:42 -0800

    nano bitmap image picture photo demo #37 - 8-bit color working

--- nano_bitmap_fullscreen.py
+++ nano_bitmap_fullscreen.py
@@ -1,23 +1,28 @@
-"""working for gray/grey scale 4-bit
+"""working for gray/grey scale 4-bit BUT not with 8-bit driver
+working color 8-bit with 8-bit driver ONLY.
 
-Not working for color
+Not working for color with 4-bit driver, end up with garbage.
 
-end up with garbage.
 Suspect need to correct palette (or load lines in correct order due to display rotation?)
 
+Grayscale/Greyscale
 
     py -3 img_cvt.py images/Philips_PM5544.pgm fullscreen.bin
 
+Color 8-bit (not 16-bit)
+
+    py -3 img_cvt.py images/Philips_PM5544.png fullscreen.bin
+
 """
 
-from color_setup import ssd  # Create a display instance
+from color_setup import ssd  # Create a display instance. NOTE edit to select 4-bit or 8-bit driver (there is no 16-bit driver)
 from gui.core.nanogui import refresh
 
 
 refresh(ssd)  # Initialise display.
 fn = "fullscreen.bin"  # Image created by `img_cvt.py`
 # The following line is required if a 4-bit driver is in use
-ssd.greyscale(True)  # NOTE if omitted, will get some colors - pallete undefined?
+#ssd.greyscale(True)  # NOTE if omitted, will get some colors - pallete undefined? NOTE2 - MISSING from 8-bit driver
 with open(fn, "rb") as f:
     _ = f.read(4)  # Read and discard rows and cols ... or
     #rows = int.from_bytes(f.read(2), "big")
