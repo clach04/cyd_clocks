6654c2f24cda07327fa908c208e5fa1d5e753cad
clach04 <clach04@gmail.com>
2025-03-09 17:11:23 -0700

    Add color versus mono display detection for nano_mono_test.py

--- nano_mono_test.py
+++ nano_mono_test.py
@@ -22,11 +22,24 @@ refresh(ssd, True)  # Initialise and clear display.
 
 For CYD - assumes USB port(s) on right hand-side
 """
-fg = WHITE  # For now assume White  # TODO figure out what color to use for background and foreground
-bg = BLACK  # default for now
+try:
+    bpp = len(ssd.mvb) // (ssd.width * ssd.height // 8)  # Bits Per Plane, i.e. color/bit-depth
+except AttributeError:
+    # probably mvb missing, assume 1-bit
+    bpp = 1
+if bpp == 1:
+    # pick the defaults for the hardware
+    # this only works for mono-eink, for color (4-bit) this will go badly
+    # most einks use white as the default background, the inverse of TFT
+    fg = 0
+    bg = 1
+else:
+    fg = WHITE
+    bg = BLACK  # reasonable for most TFT screens
 
 ssd.fill(bg)
 ssd.line(0, 0, ssd.width - 1, ssd.height - 1, fg)  # fg diagonal corner-to-corner  # TODO should this go corner to corner of screen or from inner-corner of squares?
 ssd.rect(0, 0, 15, 15, fg, True)  # fg square filled at top left
 ssd.rect(ssd.width -15, ssd.height -15, 15, 15, fg, False)  # fg square outline at bottom right
 refresh(ssd)
+# ssd.wait_until_ready()  # TODO?
